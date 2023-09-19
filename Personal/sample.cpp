3ededa5347d043d91f0312ca82f62f79e4d5eebc
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2023-09-19 14:15:24 +0530

    Update sample.cpp

--- Personal/sample.cpp
+++ Personal/sample.cpp
@@ -1,50 +1,63 @@
-#include<iostream>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
 using namespace std;
-int main(){
-    float a,b,d;
-    char c;
-    aa:
-    cout<<"Enter 1st Number :- "<<endl;
-    cin>>a;
-    cout<<"Enter 2nd Number :- "<<endl;
-    cin>>b;
-    ab:
-    cout<<"enter a operation(+,-,*,/) :- "<<endl;
-    cin>>c;
-    switch(c){
-        case '+':
-        d=a+b;
-        cout<<a<<" + "<<b<<" = "<<d<<endl;
-        break;
-        case '-':
-        d=a-b;
-        cout<<a<<" - "<<b<<" = "<<d<<endl;
-        break;
-        case '*':
-        d=a*b;
-        cout<<a<<" * "<<b<<" = "<<d<<endl;
-        break;
-        case '/':
-        d=a/b;
-        cout<<a<<" / "<<b<<" = "<<d<<endl;
-        break;
-        default:
-        cout << "Not a valid operator. Do you want to try again ?[Y/N]:- "<<endl;
-        cin>>c;
-        if(c=='y' || c=='Y'){
-            cout << "Do you want to enter new numbers ?[Y/N]"<<endl;
-            cin>>c;
-            if(c=='y' || c=='Y'){
-                goto aa;
+bool main()
+{
+
+    string s = "({[]}){()}}";
+    unordered_map<char, int> counter;
+
+    counter['('] = 0;
+    counter['{'] = 0;
+    counter['['] = 0;
+
+    vector<int> set_diff_ind;
+    char d;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        char c = s[i];
+        if (c == '(' || c == '{' || c == '[')
+        {
+            counter[c]++;
+        }
+        else if (c == ')' || c == '}' || c == ']')
+        {
+            if (c == ')')
+            {
+                d = '(';
             }
-            else{
-                goto ab;
+            else if (c == '}')
+            {
+                d = '{';
+            }
+            else
+            {
+                d = '[';
+            }
+            counter[d]--;
+            if (counter[d] == (-1))
+            {
+                return false;
+            }
+            if (counter['('] == 0 && counter['{'] == 0 && counter['['] == 0 && i != 0)
+            {
+                set_diff_ind.push_back(i);
             }
         }
-        else{
-            cout<<"Thanks for operating me (-:"<<endl;
+        else
+        {
+            return false;
         }
-        break;
     }
-    return 0;
+
+    vector<int> diff_ind;
+    for (int i = 0; i < set_diff_ind.size() - 1; i++)
+    {
+        diff_ind.push_back(set_diff_ind[i + 1] - set_diff_ind[i]);
+    }
+
+    
 }
