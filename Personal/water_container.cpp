3bfabe13c37c04be19233ccf0e547827a2109cd5
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2023-10-29 18:26:45 +0530

    water container problem done

--- Personal/water_container.cpp
+++ Personal/water_container.cpp
@@ -7,10 +7,21 @@ int main()
 {
     vector<int> height = {1,8,6,2,5,4,8,3,7};
     int g,l,r,p;
+    l=0;
+    r=height.size()-1;
     g=0;
     while(l<r){
         p=min(height[l],height[r])*(r-l);
-
+        if(p>g){
+            g=p;
+        }
+        if(height[l]==min(height[l],height[r])){
+            l++;
+        }
+        else{
+            r--;
+        }
     }
+    cout<<g;
     return 0;
 }
