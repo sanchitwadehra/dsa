8de6173a6625a64e6ba981b63bb83ed2c0c9df53
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2023-10-29 18:28:34 +0530

    Update trapping_rain_water.cpp

--- Personal/trapping_rain_water.cpp
+++ Personal/trapping_rain_water.cpp
@@ -6,6 +6,12 @@ using namespace std;
 int main()
 {
     vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
-    
+    int l,r,s,t;
+    l=0;
+    r=0;
+    s=0;
+    for(r=0;r<height.size();r++){
+        
+    }
     return 0;
 }
