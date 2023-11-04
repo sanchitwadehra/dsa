2423ae1fc121cf7a9bf54af52ddc8517ee7edb49
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2023-11-04 15:30:13 +0530

    Update longest_repeating_character_replacement.cpp

--- Personal/longest_repeating_character_replacement.cpp
+++ Personal/longest_repeating_character_replacement.cpp
@@ -1,31 +1,20 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <unordered_set>
 
-int longest_repeating_character_replacement(std::string s,int k) {
-    int n = s.length();
-    int maxLength = 0;
-    int left = 0, right = 0;
-    std::unordered_set<char> uniqueChars;
-
-    while (right < n) {
-        if (uniqueChars.find(s[right]) == uniqueChars.end()) {
-            uniqueChars.insert(s[right]);
-            maxLength = std::max(maxLength, right - left + 1);
-            right++;
-        } else {
-            uniqueChars.erase(s[left]);
-            left++;
-        }
-    }
-
-    return maxLength;
+int longest_repeating_character_replacement(std::string s, int k)
+{
+    int s = s.size();
+    vector<char> key;
+    vector<int> value;
 }
 
-int main() {
+int main()
+{
     std::string s = "AABABBA";
-    int k = 1;
-    int result = longest_repeating_character_replacement(s,k);
+    int k = 2;
+    int result = longest_repeating_character_replacement(s, k);
     std::cout << "longest_repeating_character_replacement :- " << result << std::endl;
 
     return 0;
