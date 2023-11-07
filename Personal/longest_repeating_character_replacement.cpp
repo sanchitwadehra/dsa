440ef2f29b5e422f7644d8ba1893906c3c060c9c
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2023-11-07 07:41:07 +0530

    Update longest_repeating_character_replacement.cpp

--- Personal/longest_repeating_character_replacement.cpp
+++ Personal/longest_repeating_character_replacement.cpp
@@ -4,11 +4,31 @@
 #include <unordered_set>
 using namespace std;
 
+void most_popular_set_top(vector<pair<char, int> > &record){
+    int g;
+    g=0;
+    for(int i=0;i<record.size();i++){
+        if(record[i].second>g){
+            g=i;
+        }
+    }
+    pair<char,int> temp;
+    temp.first=record[g].first;
+    temp.second=record[g].second;
+    record.erase(record.begin()+g);
+    record.insert(record.begin(),temp);
+}
+
 int longest_repeating_character_replacement(std::string s, int k)
 {
     int s = s.size();
     vector<pair<char, int> > record;
-    
+    int l,r;
+    l=0;
+    r=0;
+    while(r<s.size()){
+
+    }
 }
 
 int main()
