d39c45ae8a0b9654f80bf896e9d1847e4fd4e7c0
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2023-11-11 00:04:19 +0530

    Update longest_repeating_character_replacement.cpp

--- Personal/longest_repeating_character_replacement.cpp
+++ Personal/longest_repeating_character_replacement.cpp
@@ -2,66 +2,92 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include<unordered_map>
+#include <unordered_map>
 using namespace std;
-
-auto compare = [](const auto& lhs, const auto& rhs) {
-        return lhs.second < rhs.second;
-    };
 
 int longest_repeating_character_replacement(string s, int k)
 {
-    unordered_map<char,int> record;
-    int l,r,g;
-    g=0;
-    l=0;
-    r=0;
+    unordered_map<char, int> record;
+    int l, r, g,gi;
+    g = 0;
+    l = 0;
+    r = 0;
+    gi=0;
     char top_element;
     int temp_top_element_value;
     int temp_k;
-    temp_k=0;
-    while(r<s.length()){
-        if(record.find(s[r])==record.end()){
-            record[s[r]]=1;
-            cout<<s[r]<<" "<<record[s[r]]<<endl;
-            if((r-l+1)>g){
-                g=(r-l+1);
+    temp_k = 0;
+
+    auto compare = [](const auto &lhs, const auto &rhs)
+    {
+        return lhs.second < rhs.second;
+    };
+
+    while (r < s.length())
+    {
+        if (r == 0)
+        {
+            if (record.find(s[r]) == record.end())
+            {
+                record[s[r]] = 1;
+                if ((r - l + 1) > g)
+                {
+                    g = (r - l + 1);
+                    gi=l;
+                }
+                r++;
             }
-            temp_k++;
+        }
+        else if (record.find(s[r]) == record.end() && (temp_k + 1) <= k)
+        {
+            record[s[r]] = 1;
+            if ((r - l + 1) > g)
+            {
+                g = (r - l + 1);
+                gi=l;
+            }
             r++;
         }
-        else if(s[r]==top_element || (temp_k+1)<=k){
-            if(s[r]==top_element){
+        else if (s[r] == top_element || (temp_k + 1) <= k)
+        {
+            if (s[r] == top_element)
+            {
                 record[top_element]++;
-                temp_top_element_value=record[top_element];
             }
-            else{
+            else
+            {
                 record[s[r]]++;
-                temp_k++;
             }
-            if((r-l+1)>g){
-                g=(r-l+1);
+            if ((r - l + 1) > g)
+            {
+                g = (r - l + 1);
+                gi=l;
             }
             r++;
         }
-        else{
+        else
+        {
             record[s[l]]--;
-            if((r-l+1)>g){
-                g=(r-l+1);
+            if (s[l] == 0)
+            {
+                record.erase(s[l]);
             }
             l++;
+            record[s[r]] = 1;
             r++;
         }
         auto it = max_element(record.begin(), record.end(), compare);
-        top_element=it->first;
+        top_element = it->first;
+        temp_k = r - record[top_element];
     }
+    cout<<"gi "<<gi<<endl;
     return g;
 }
 
 int main()
 {
-    string s = "AABABBA";
-    int k = 2;
+    string s = "ACBAAAABBA";
+    int k = 1;
     int result = longest_repeating_character_replacement(s, k);
     cout << "longest_repeating_character_replacement :- " << result << endl;
 
