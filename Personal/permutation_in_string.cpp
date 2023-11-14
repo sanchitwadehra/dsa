2245ca5d7c1656102e9d8774d4baa4a12a1626d4
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2023-11-14 13:38:04 +0530

    Update permutation_in_string.cpp

--- Personal/permutation_in_string.cpp
+++ Personal/permutation_in_string.cpp
@@ -7,65 +7,38 @@ using namespace std;
 
 bool permutation_in_string(string s1, string s2)
 {
-    unordered_map<char, int> rs1;
-    unordered_map<char, int> rs2;
-    int l, r, n;
-    n = s1.length();
-    l = 0;
-    r = l + (n - 1);
+    unordered_map<char, int> check;
+    unordered_map<char, int> record;
+    int l, r;
+    l=0;
+    r=0;
     for (int i = 0; i < s1.length(); i++)
-    {
-        rs1[s1[i]]++;
+    {   
+        check[s1[i]]++;
     }
     if (s1.length() > s2.length())
     {
         return false;
     }
-    else
-    {
-        while (r < s2.length() && l < s2.length())
-        {
-            for (int i = l; i <= r; i++)
-            {
-                rs2[s2[i]]++;
-                if (rs2[s2[i]] == rs1[s2[i]] || rs2[s2[i]] < rs1[s2[i]])
-                {
-                    if (i == r)
-                    {
-                        return true;
-                    }
-                }
-                else
-                {
-                    if (rs2[s2[i]] > 1)
-                    {
-                        for (int j = i; j >= l; j--)
-                        {
-                            if (s2[j] == s2[i])
-                            {
-                                if (rs2[s2[j]] == rs1[s2[j]])
-                                {
-                                    l = j;
-                                    r = (l + 1) + (n - 1);
-                                    rs2.clear();
-                                    break;
-                                }
-                            }
-                            rs2[s2[j]]--;
-                        }
-                    }
-                    else
-                    {
-                        l = i + 1;
-                        r = l + (n - 1);
-                        rs2.clear();
+    else{
+        while(r<s2.length()){
+            record[s2[r]]++;
+            if(record[s2[r]]>check[s2[r]]){
+                while(l<=r){
+                    record[s2[l]]--;
+                    l++;
+                    if(record[s2[r]]==check[s2[r]]){
                         break;
                     }
                 }
             }
+            if((r-l+1)==s1.length()){
+                return true;
+            }
+            r++;
         }
+        return false;
     }
-    return false;
 }
 
 int main()
