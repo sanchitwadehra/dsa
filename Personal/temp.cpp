368e140024265dd989d1e71063828648717afcf5
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2023-11-14 16:57:29 +0530

    Update temp.cpp

--- Personal/temp.cpp
+++ Personal/temp.cpp
@@ -1,23 +1,51 @@
 #include <iostream>
-#include <unordered_map>
-#include <climits>
+#include <string>
+#include <vector>
 #include <algorithm>
+#include <unordered_map>
 using namespace std;
 
-auto compare = [](const auto& lhs, const auto& rhs) {
-        return lhs.second < rhs.second;
-    };
+bool minimum_window_substring(string t, string s)
+{
+    unordered_map<char, int> check;
+    unordered_map<char, int> record;
+    int l, r;
+    l=0;
+    r=0;
+    for (int i = 0; i < t.length(); i++)
+    {   
+        check[t[i]]++;
+    }
+    if (t.length() > s.length())
+    {
+        return false;
+    }
+    else{
+        while(r<s.length()){
+            record[s[r]]++;
+            if(record[s[r]]>check[s[r]]){
+                while(l<=r){
+                    record[s[l]]--;
+                    l++;
+                    if(record[s[r]]==check[s[r]]){
+                        break;
+                    }
+                }
+            }
+            if((r-l+1)==t.length()){
+                return true;
+            }
+            r++;
+        }
+        return false;
+    }
+}
 
-int main() {
-    unordered_map<char, int> record = {{'A', 2}, {'B', 1}};
-    char top_element;
-
-    
-
-    auto it = max_element(record.begin(), record.end(), compare);
-    top_element = it->first;
-
-    cout << "Key jiska max value hai: " << top_element << endl;
-
+int main()
+{
+    string t = "hello";
+    string s = "ooolleoooleh";
+    bool result = minimum_window_substring(t, s);
+    cout << "minimum_window_substring :- " << result << endl;
     return 0;
 }
