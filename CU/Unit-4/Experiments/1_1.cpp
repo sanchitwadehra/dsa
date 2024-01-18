ec049de4a1807d19e6b98b4717d5d1c8004f8ab1
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2024-01-18 14:05:55 +0530

    quick sort experiment done

---
+++ CU/Unit-4/Experiments/1_1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int low, int high, int target) {
+    if (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+
+        if (arr[mid] > target)
+            return binarySearch(arr, low, mid - 1, target);
+
+        return binarySearch(arr, mid + 1, high, target);
+    }
+
+    return -1;  // agar element nahi mila
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 7;
+
+    int result = binarySearch(arr, 0, n - 1, target);
+
+    if (result != -1)
+        cout << "Element " << target << " found at index " << result << endl;
+    else
+        cout << "Element " << target << " not found in the array" << endl;
+
+    return 0;
+}
